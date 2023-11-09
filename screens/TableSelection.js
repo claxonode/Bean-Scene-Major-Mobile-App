b4bb4139d3731c20188c3bd3c12f34c63b466b86
Zac Chalmers <zac.chalmers@studytafensw.edu.au>
2023-11-09 15:31:16 +1100

    Tables can now be sorted by their area.
    Added styles for the buttons

--- screens/TableSelection.js
+++ screens/TableSelection.js
@@ -1,7 +1,8 @@
 import {useFocusEffect, useNavigation} from '@react-navigation/native';
 import React, { useEffect, useState} from 'react';
-import { View, Text, Button, SectionList } from 'react-native';
+import { View, Text, Button, SectionList, SafeAreaView, StyleSheet, Pressable } from 'react-native';
 import {getAllTables} from "../services/TableApiService"
+import { SegmentedButtons } from 'react-native-paper';
 
 
 
@@ -9,13 +10,15 @@ export default function TableSelection() {
   const navigation = useNavigation();
   const [tables, setTables] = useState([]);
   const [selectedTable, setSelectedTable] = useState(null);
+  const [currentArea,setCurrentArea] = useState("Main")
 
-  function onTableSelect(selectedTable) {
+  const onTableSelect = (selectedTable) => {
     // Handle the selected table
     console.log('Selected table:', selectedTable);
     // Add your logic for handling the selected table
     navigation.navigate('MenuList', { selectedTable });
   };
+
   useEffect(() => {
     async function fetchData() {
       try {
@@ -31,41 +34,92 @@ export default function TableSelection() {
 
   const handleTableSelect = (table) => {
     setSelectedTable(table);
-    onTableSelect(table); // Pass the selected table back to the parent component
+    onTableSelect(table);
   };
 
-  // Group tables by area
-  const groupedTables = tables.reduce((acc, table) => {
-    acc[table.area] = acc[table.area] || [];
-    acc[table.area].push(table);
-    return acc;
-  }, {});
+ 
+  const groupTablesByArea = () => {
+    const groupedTables = tables.reduce((acc, table) => {
+      acc[table.area] = acc[table.area] || [];
+      acc[table.area].push(table);
+      return acc;
+    }, {});
 
-  const sections = Object.keys(groupedTables).map((area) => ({
-    title: area,
-    data: groupedTables[area],
-  }));
+    if (currentArea === 'All') {
+      return Object.keys(groupedTables).map((area) => ({
+        title: area,
+        data: groupedTables[area],
+      }));
+    } else {
+      return [
+        {
+          title: currentArea,
+          data: groupedTables[currentArea] || [],
+        },
+      ];
+    }
+  };
 
   return (
     <View>
+      <SafeAreaView >
+        <SegmentedButtons
+          value={currentArea}
+          onValueChange={setCurrentArea}
+          buttons={[
+            {
+              value: 'All',
+              label: 'All'
+            },
+            {
+              value: 'Main',
+              label: 'Main'
+            },
+            {
+              value: 'Balcony',
+              label: 'Balcony'
+            },
+            {
+              value: 'Outside',
+              label: 'Outside'
+            }
+            
+          ]}
+        />
+      </SafeAreaView>
+
       <SectionList
-        sections={sections}
+        sections={groupTablesByArea()}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
-          <Button
-            title={`Table ${item.id}: ${item.name}`}
-            onPress={() => handleTableSelect(item)}
-          />
+          <Pressable style={styles.button} onPress={() => handleTableSelect(item)}>
+           <Text style={styles.text}>Table {item.name}: {item.area} area</Text>           
+          </Pressable>
         )}
         renderSectionHeader={({ section: { title } }) => (
-          <Text style={{ fontWeight: 'bold' }}>{title}</Text>
+          <Text style={{ fontWeight: 'bold' }}>{title} Area</Text>
         )}
       />
-      {selectedTable && (
-        <Text>
-          Selected Table: Table {selectedTable.id} - {selectedTable.name} in {selectedTable.area}
-        </Text>
-      )}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: '#b27b43',
+    margin: 3
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+});
